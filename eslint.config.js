4a826102db447c8ef6bfbc35c464e327f2cd07a5
Gin Song <sjgaru@gmail.com>
2024-12-08 04:25:21 +0900

    👷 #1 프리티어 설정

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+    },
+  },
+];
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import { useState } from "react";
-import TinderCard from "react-tinder-card";
-import "./App.css";
+import { useState } from 'react';
+import TinderCard from 'react-tinder-card';
+import './App.css';
 
 function App() {
   const [people, setPeople] = useState([
     {
-      name: "1",
-      url: "https://blog-static.kkday.com/ko/blog/wp-content/uploads/malaysia_kota_kinabalu_pulau_tiga.jpg",
+      name: '1',
+      url: 'https://blog-static.kkday.com/ko/blog/wp-content/uploads/malaysia_kota_kinabalu_pulau_tiga.jpg',
     },
     {
-      name: "2",
-      url: "https://cdn.informaticsview.com/news/photo/202408/485_1741_4410.jpg",
+      name: '2',
+      url: 'https://cdn.informaticsview.com/news/photo/202408/485_1741_4410.jpg',
     },
     {
-      name: "3",
-      url: "https://t1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/1jPF/image/oa_fqRsHcmBRlEPCIjSKOHDW_gs.jpg",
+      name: '3',
+      url: 'https://t1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/1jPF/image/oa_fqRsHcmBRlEPCIjSKOHDW_gs.jpg',
     },
   ]);
 
   const onSwipe = (direction: string, nameToDelete: string) => {
-    console.log("방향: " + direction + ", 삭제된 사용자: " + nameToDelete);
+    console.log('방향: ' + direction + ', 삭제된 사용자: ' + nameToDelete);
     // 실제로 카드를 제거하는 로직 추가
     setPeople((prevPeople) => prevPeople.filter((person) => person.name !== nameToDelete));
   };
@@ -31,14 +31,16 @@ function App() {
           <TinderCard
             key={person.name}
             className="swipe"
-            onSwipe={(dir) => onSwipe(dir, person.name)}>
+            onSwipe={(dir) => onSwipe(dir, person.name)}
+          >
             <div
               className="card"
               style={{
                 backgroundImage: `url(${person.url})`,
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-              }}>
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+              }}
+            >
               <h3>{person.name}</h3>
             </div>
           </TinderCard>

706eae47ed64dcffa9ee3290c4e1f62e72b91ebc
Gin Song <164488959+sjgaru-dev@users.noreply.github.com>
2024-12-08 04:37:00 +0900

    👷 #1 기본환경 설정 테스트 (#3)
    * test
    
    * í¾ #1 기본환경 설정
    
    * ✨ #1 테스트 추가
    
    * 👷 #1 lint 설정
    
    * 👷 #1 깃모지 설정 완료
    
    * 👷 #1 프리티어 설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,103 +1,38 @@
-module.exports = {
-  root: true,
-  env: { browser: true, es2021: true, node: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended',
-    'plugin:import/recommended',
-    'plugin:import/typescript',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint', 'react', 'prettier', 'import', 'import-order'],
-  rules: {
-    'no-console': ['warn', { allow: ['warn', 'error'] }],
-    'react/function-component-definition': [
-      2,
-      {
-        namedComponents: 'arrow-function',
-        unnamedComponents: 'arrow-function',
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
       },
-    ],
-    'arrow-body-style': ['error', 'as-needed'],
-    'react/jsx-pascal-case': 'error',
-    camelcase: ['error', { properties: 'never' }],
-    'react/jsx-handler-names': [
-      'error',
-      {
-        eventHandlerPropPrefix: 'on',
-      },
-    ],
-    'import/no-default-export': 'off',
-    'import/prefer-default-export': 'off',
-    'import/no-unresolved': 'error',
-    'react-hooks/rules-of-hooks': 'error',
-    'react/prop-types': 'warn',
-    'prefer-const': 'error',
-    'no-var': 'error',
-    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-    eqeqeq: 'error',
-    'prefer-destructuring': [
-      'error',
-      {
-        array: false,
-        object: true,
-      },
-    ],
-    'import/order': [
-      'error',
-      {
-        groups: ['builtin', 'external', 'internal', 'index'],
-        pathGroups: [
-          {
-            pattern: 'react',
-            group: 'external',
-            position: 'before',
-          },
-        ],
-        pathGroupsExcludedImportTypes: ['react'],
-        'newlines-between': 'always',
-        alphabetize: {
-          order: 'asc',
-          caseInsensitive: true,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
         },
       },
-    ],
-    'react/jsx-no-duplicate-props': ['error', { ignoreCase: true }],
-    '@typescript-eslint/no-unused-vars': [
-      'warn',
-      {
-        varsIgnorePattern: '^_',
-        argsIgnorePattern: '^_',
-      },
-    ],
-    '@typescript-eslint/no-empty-interface': 'off',
-    '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    'no-useless-rename': 'error',
-    'object-shorthand': 'error',
-    'react/jsx-key': ['error', { checkFragmentShorthand: true }],
-    'react/react-in-jsx-scope': 'off',
-  },
-  settings: {
-    react: {
-      version: 'detect',
     },
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-        paths: ['src'],
+    settings: {
+      react: {
+        version: 'detect',
       },
     },
   },
-};
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+    },
+  },
+];
